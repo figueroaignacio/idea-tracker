8f1269e42d0ae22dc945483daca3228b8e128e41
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-03-21 22:42:08 -0300

    fix: login page redirect to home page

--- apps/client/app/layout/auth-layout.tsx
+++ apps/client/app/layout/auth-layout.tsx
@@ -1,10 +1,14 @@
-import { Outlet } from "react-router";
-import { BackButton } from "~/components/back-button";
+import { Link, Outlet } from "react-router";
 
 export default function AuthLayout() {
   return (
     <div className="container">
-      <BackButton />
+      <Link
+        to="/"
+        className="border-[1px] border-white/15 px-4 py-2 rounded-xl cursor-pointer"
+      >
+        👈🏽 Back
+      </Link>
       <Outlet />
     </div>
   );
