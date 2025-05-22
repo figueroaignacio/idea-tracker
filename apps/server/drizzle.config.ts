44b3a8299f256e26f6c09d0a052d7340361e9dec
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-05-22 18:44:01 -0300

    fix: db connection

--- apps/server/drizzle.config.ts
+++ apps/server/drizzle.config.ts
@@ -1,15 +1,18 @@
 import { config } from 'dotenv';
 config();
 
+import type { Config } from 'drizzle-kit';
+
 export default {
   schema: './src/db/schema.ts',
   out: './drizzle',
-  driver: 'pg',
+  dialect: 'postgresql',
   dbCredentials: {
     host: process.env.DB_HOST!,
     port: Number(process.env.DB_PORT!),
     user: process.env.DB_USER!,
     password: process.env.DB_PASS!,
     database: process.env.DB_NAME!,
+    ssl: false,
   },
-};
+} satisfies Config;
