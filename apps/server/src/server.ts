780dd83f74ee0c00882aed85a5f93d71aecb7b39
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-06-14 22:35:02 -0300

    chore: remove main projects

--- apps/server/src/server.ts
+++
@@ -1,7 +0,0 @@
-import { App } from './app';
-import { config } from './config';
-import { logger } from './utils/logger';
-
-const app = new App();
-app.listen(config.app.port);
-logger.info(`Running ${config.app.name} v${config.app.version} on port ${config.app.port}`);
